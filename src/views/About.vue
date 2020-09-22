f9baf178ea0aa81cfc9369f07dc77acd9cc9b9c9
Matheus Motta <mat-motta@hotmail.com>
2020-09-22 17:07:13 -0300

    Primeiro commit

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <base-button type="primary">Button</base-button>
+      <base-button type="primary" icon="ni ni-bag-17">With icon</base-button>
+      <base-button type="primary" icon="ni ni-atom"></base-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BaseButton from '@/components/BaseButton.vue'
+
+export default {
+  name: 'About',
+  components: {
+    BaseButton
+  }
+}
+</script>
